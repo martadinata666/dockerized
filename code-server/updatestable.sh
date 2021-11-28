0a3bcf9ecb4256b53c9a0faab4b1decf8b95b5c7
Dedy Martadinata Supriyadi <dedyms@hotmail.com>
2021-11-28 16:13:53 +0700

    remove somefiles

--- code-server/updatestable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -X POST \
-     -F token=9ad370d22dda5bc236f9aa030b699c \
-     -F "ref=master" \
-     -F "variables[CODE_RELEASE]=3.12.0" \
-     https://gitlab.com/api/v4/projects/25438334/trigger/pipeline
--- stash/updatestable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -X POST \
-     -F token=854853555575e0ec77e240feb37e44 \
-     -F "ref=master" \
-     -F "variables[VERSION]=v0.10.0" \
-     https://gitlab.com/api/v4/projects/27566180/trigger/pipeline
