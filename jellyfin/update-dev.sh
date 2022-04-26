00d1e403d6fd74d98a977376f5c3ccf647ac56ab
Dedy Martadinata Supriyadi <dedyms@hotmail.com>
2022-04-26 10:14:15 +0700

    Update

--- jellyfin/update-dev.sh
+++ jellyfin/update-dev.sh
@@ -2,7 +2,7 @@
 curl -X POST \
      -F token=1aa8a4cbe75e45b34dfb50c5088d4a \
      -F ref=master \
-     -F "variables[DEV]=dev-web" \
+     -F "variables[DEV]=dev-both" \
      http://192.168.0.2:10000/api/v4/projects/60/trigger/pipeline
 #dev-web
 #dev-dotnet
