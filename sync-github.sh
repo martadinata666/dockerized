7c585a33cf2886293c830bb342177fa4e9e18dba
Dedy Martadinata Supriyadi <dedyms@proton.me>
2023-11-06 14:14:53 +0700

    Update

--- sync-github.sh
+++ sync-github.sh
@@ -2,6 +2,7 @@
 # sed -i '1i\#syntax=docker/dockerfile:1.4' ./*/*/Dockerfile*
 git-disable-repo.sh --true && \
 mv ./..git ./.git && \
+git fetch && \
 git add . && \
 git commit -m "Update" && \
 git push origin master && \
