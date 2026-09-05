4764b6c98b2d7239d245134924d4e803c1e9ded3
Dedy Martadinata Supriyadi <dedyms@proton.me>
2026-09-05 10:49:38 +0700

    nextcloud local build

--- unmanic/make-image.sh
+++ unmanic/make-image.sh
@@ -1,1 +1,1 @@
-docker buildx build . -f Dockerfile -t 192.168.0.2:6060/dedyms/unmanic:git --push
+docker buildx build --build-arg RELEASE=staging . -f Dockerfile -t 192.168.0.2:6060/dedyms/unmanic:git --push
